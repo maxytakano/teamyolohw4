924be1dbc10ded20fa8a842216226620c2444a2b
Jason Tan <jastan313@gmail.com>
2015-11-30 18:17:07 -0800

    added user/habit analytics

--- js/global.js
+++ js/global.js
@@ -3,6 +3,50 @@ function initParse() {
     return true;
 }
 
+function javascriptErrorAnalytics() {
+    window.onerror = function(errorMsg, url, lineNumber, column) {
+        var dimensions = {
+            error_msg: JSON.stringify(errorMsg),
+            line_number: JSON.stringify(lineNumber),
+            column_number: JSON.stringify(column)
+        };
+
+        Parse.Analytics.track('JSErrorAnalytics', dimensions);
+        return false;
+    }
+}
+
+function userAnalytics(operation, username, error) {
+    var dimensions = {
+        operation_name: operation,
+        user_name: username,
+        error_code: error
+    };
+    console.log(dimensions);
+    Parse.Analytics.track('UserAnalytics', dimensions);
+}
+
+function habitCRUDAnalytics(operation, username, habitname, error) {
+    var dimensions = {
+        operation_name: operation,
+        user_name: username,
+        habit_name: habitname,
+        error_code: error
+    };
+    console.log(dimensions);
+    Parse.Analytics.track('HCRUDAnalytics', dimensions);
+}
+
+function habitInteractionAnalytics(operation, username, habit, error) {
+    var dimensions = {
+        operation_name: operation,
+        user_name: username,
+        habit: habit,
+        error_code: error
+    };
+    Parse.Analytics.track('HIAnalytics', dimensions);
+}
+
 // Checks if an user is logged on already.
 function checkCurrentUser() {
     // If user has not logged in or previous user
@@ -24,6 +68,8 @@ function checkCurrentUser() {
 // Logs out the current user
 function logOut() {
     if(!confirm("Are you sure you want to log out?")){ return; }
+    var currentUser = Parse.User.current();
+    userAnalytics('Logout', currentUser.get('username'), '');
     Parse.User.logOut();
     if(!(/login.html/.test(window.location.href))) {
         window.location = 'login.html';
